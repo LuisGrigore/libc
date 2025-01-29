568662eaa443c0193ef92912d0dc4b5b1f5561cf
Luis <luiscristiangrigore11@gmail.com>
2025-01-29 03:47:50 +0100

    ggg

--- ft_split.c
+++ ft_split.c
@@ -2,23 +2,38 @@
 #include <stdio.h>
 
 char *substr(char *str, int start, int end);
-int count_instances(char* str, char c);
-int is_begin(char* str, char c);
-int is_end(char* str, char c);
+char **add_to_split_array(char ** src, char* str, int size);
 
 char **ft_split(char const *s, char c)
 {
 	int i = 0;
 	int j = 0;
-	char str = s;
+	int res_size = 1;
+	char *str = s;
+	char ** result = (char **) malloc(sizeof(char*));
+
 	while(str[j] != '\0')
 	{
-		if(str[j] == c)
+		if(str[j] == c || str[j+1] == '\0')
 		{
 			if(i != j)
 			{
-				//añadir string al arry
-				printf(substr(str,i,j-1));
+				if(str[j+1] == '\0')
+				{
+					printf("%s\n",substr(str,i,j+1));
+					printf("%d\n",res_size);
+					result = add_to_split_array(result,substr(str,i,j+1),res_size);
+					printf("%d",result);
+
+				}
+				else
+				{
+					printf("%s\n",substr(str,i,j));
+					printf("%d\n",res_size);
+					result = add_to_split_array(result,substr(str,i,j),res_size);
+					printf("%d",result);
+				}
+				res_size++;
 			}
 			i = j;
 			i++;
@@ -30,75 +45,58 @@ char **ft_split(char const *s, char c)
 		}
 
 	}
-	
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int is_begin(char* str, char c)
-{
-	if(str[0] == c)
-	{
-		return 1;
-	}
-	return 0;
-}
-
-int is_end(char* str, char c)
-{
-	int i = 0;
-	while(str[i] != '\0')
-	{
-		i++;
-	}
-	if(str[i-1] == c)
-	{
-		return 1;
-	}
-	return 0;
-}
-
-int count_instances(char* str, char c)
-{
-	int i = 0;
-	int result = 0;
-	while(str[i] != '\0')
-	{
-		if(str[i] == c)
-		{
-			result ++;
-		}
-		i++;
-	}
-	return result;
-}
 
 char *substr(char *str, int start, int end)
 {
 	int size = end-start;
-	char *result = (char*) malloc(size * sizeof(char));
+	char *result = (char*) malloc(size + 1 * sizeof(char));
 	int i = 0;
 	while(i < size)
 	{
 		result[i] = str[start + i];
 		i ++;
 	}
+	result[size] = '\0';
+	return result;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	int i;
+	char *s;
+	char *d;
+
+	i = 0;
+	s = (char *) src;
+	d = (char *) dest;
+
+	if(src < dest)
+	{
+		while(i < n)
+		{
+			d[(n-1)-i] = s[(n-1)-i];
+			i++;
+		}
+	}
+	else
+	{
+		while(i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return dest;
+}
+char **add_to_split_array(char ** src, char* str, int size)
+{
+	char **result;
+	result = (char **) malloc((size + 1) * sizeof(char *));
+	memmove(result,src, size);
+	result[size] = str;
+	free(src);
 	return result;
 }
--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char const *argv[])
 {
-	char *a = " Hola que tal";
+	char *a = "Hola  que    tal ";
 	char ** b = ft_split(a, ' ');
 	//char * c = substr(a,1,4);
-	//printf("c: %s", b[0]);
+	printf("c: %s", b[0]);
 	return 0;
 }
