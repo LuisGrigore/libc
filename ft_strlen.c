1283217ebd921b381f49615a5a2fa1d6171f9b5a
Luis Cristian Grigore Grigore <lgrigore@c2r12s2.42madrid.com>
2025-01-31 18:28:06 +0100

    norminetted

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lgrigore <lgrigore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:20:56 by lgrigore          #+#    #+#             */
-/*   Updated: 2025/01/31 13:21:03 by lgrigore         ###   ########.fr       */
+/*   Updated: 2025/01/31 17:03:49 by lgrigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen(const char *s)
 {
-	int	num;
+	size_t	num;
 
 	num = 0;
 	if (s == NULL)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgrigore <lgrigore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:26:13 by lgrigore          #+#    #+#             */
-/*   Updated: 2025/01/31 14:59:54 by lgrigore         ###   ########.fr       */
+/*   Updated: 2025/01/31 17:32:03 by lgrigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,17 @@ char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char *ft_strnstr(const char *big, const char *little, size_t len);
 /*
-ft_strnstr
 ft_atoi
 ft_calloc
 ft_strdup*/
 char	**ft_split(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
