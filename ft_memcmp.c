11b48a6c99441afc4efec5ce58f9213027758eec
Luis Cristian Grigore Grigore <lgrigore@c2r5s4.42madrid.com>
2025-01-31 15:22:52 +0100

    norminetted

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lgrigore <lgrigore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:11:55 by lgrigore          #+#    #+#             */
-/*   Updated: 2025/01/31 15:11:56 by lgrigore         ###   ########.fr       */
+/*   Updated: 2025/01/31 15:19:12 by lgrigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    const unsigned char *ptr1 = (const unsigned char *)s1;
-    const unsigned char *ptr2 = (const unsigned char *)s2;
+	const unsigned char	*ptr1 = (const unsigned char *)s1;
+	const unsigned char	*ptr2 = (const unsigned char *)s2;
 
-    while (n-- > 0)
-    {
-        if (*ptr1 != *ptr2)
-        {
-            return *ptr1 - *ptr2;
-        }
-        ptr1++;
-        ptr2++;
-    }
-
-    return 0; 
+	while (n-- > 0)
+	{
+		if (*ptr1 != *ptr2)
+		{
+			return (*ptr1 - *ptr2);
+		}
+		ptr1++;
+		ptr2++;
+	}
+	return (0);
 }
