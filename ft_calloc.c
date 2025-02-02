22c7b84b0fe002a270578a8da13ce08be1354f6a
Luis <luiscristiangrigore11@gmail.com>
2025-02-02 11:40:30 +0100

    ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,16 +1,18 @@
 #include "libft.h"
 void *ft_calloc(size_t nmemb, size_t size)
 {
-	if(nmemb * size == 0)
-	{
+	size_t total_size = nmemb * size;
 
-	}
-	int *ptr = (int *) malloc(nmemb * size);
-	int i = 0;
-	while(i < nmemb * size)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return (void *) ptr;
+    if (nmemb != 0 && total_size / nmemb != size)
+        return NULL;
+
+    void *ptr = malloc(total_size);
+    if (!ptr)
+        return NULL;
+
+    unsigned char *byte_ptr = (unsigned char *)ptr;
+    for (size_t i = 0; i < total_size; i++)
+        byte_ptr[i] = 0;
+
+    return ptr;
 }
