17c6cc9a096f463b5075fd3476bd8e9e09db03f9
Luis Cristian Grigore Grigore <lgrigore@c2r8s6.42madrid.com>
2025-02-03 15:01:54 +0100

    calloc arreglado??

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: lgrigore <lgrigore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 12:49:45 by lgrigore          #+#    #+#             */
-/*   Updated: 2025/02/03 13:59:42 by lgrigore         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:00:43 by lgrigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -23,8 +22,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	i = 0;
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
-	if (total_size == 0)
-		return (NULL);
 	if (nmemb != 0 && total_size / nmemb != size)
 		return (NULL);
 	if (!ptr)
@@ -39,3 +36,18 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	}
 	return (ptr);
 }
+
+/*void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	tsize;
+
+	tsize = nmemb * size;
+	if (nmemb != 0 && tsize / nmemb != size)
+		return (NULL);
+	ptr = malloc(tsize);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, tsize);
+	return (ptr);
+}*/
